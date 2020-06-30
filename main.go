eb179c432f3f7c2eae2e7920e8eb7e3f771c6f83
Alcheming <sanghun219@naver.com>
2020-06-30 15:38:16 +0900

    #1 Url Checker (finish)

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/learngo/bbd/mydict"
+	"net/http"
 )
 
+var errRequestFail = errors.New("Request Fail")
+
 func main() {
-	dict := mydict.Dictionary{}
-	err := dict.Add("hello", "greeting")
-	if err != nil {
-		fmt.Println("error")
+	var result = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	word, err := dict.Search("hello")
-	fmt.Println(word)
+
+	for _, url := range urls {
+		res := "OK"
+		err := hitURL(url)
+		if err != nil {
+			res = "FAIL"
+		}
+		result[url] = res
+	}
+	for url, res := range result {
+		fmt.Println(url, res)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking URL : ", url)
+	res, err := http.Get(url)
+	if err != nil || res.StatusCode >= 400 {
+		return errRequestFail
+	} else {
+		return nil
+	}
+
 }
