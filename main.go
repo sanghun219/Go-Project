49e18e87af9e9fbbd0043ac55af1d780ae28ecad
Alcheming <sanghun219@naver.com>
2020-06-30 17:04:57 +0900

    #2 Goroutine & URL Checker(Finished)

--- main.go
+++ main.go
@@ -1,25 +1,54 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func main() {
+type result struct {
+	url    string
+	status string
+}
 
-	c := make(chan bool)
-	people := [5]string{"sang", "jang", "pang", "kong", "tong"}
-	for _, person := range people {
-		go isSexy(person, c)
+var errRequestFail = errors.New("Request Fail")
+
+func main() {
+	var results = make(map[string]string)
+	ReceiveResult := make(chan result)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
 
-	for i := 0; i < len(people); i++ {
-		fmt.Println(<-c)
+	for _, url := range urls {
+		go hitURL(url, ReceiveResult)
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-ReceiveResult
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
+
 	}
 }
 
-func isSexy(person string, c chan bool) {
-	time.Sleep(time.Second * 5)
-	fmt.Println(person)
-	c <- true
+func hitURL(url string, c chan<- result) {
+	res, err := http.Get(url)
+	if err != nil || res.StatusCode >= 400 {
+		c <- result{url: url, status: "FAIL"}
+	} else {
+		c <- result{url: url, status: "SUCCESS"}
+	}
+
 }
